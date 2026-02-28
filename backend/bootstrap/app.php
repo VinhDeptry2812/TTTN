0d2d64127feed7c1f20e649e485471316ba52e77
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-01 02:54:19 +0700

    lai fix

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(TrustProxies::class);
         $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
