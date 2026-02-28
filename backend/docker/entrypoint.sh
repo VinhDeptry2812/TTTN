14a6e4f5f7920b6dd7f04f9fca9d1d485be1c46a
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-01 02:40:17 +0700

    fixxx

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -15,6 +15,7 @@ php artisan config:clear
 php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
+php artisan optimize:clear
 
 echo "🚀 Starting server..."
 php artisan serve --host=0.0.0.0 --port=10000
