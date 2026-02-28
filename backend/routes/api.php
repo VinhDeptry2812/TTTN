b8db6c54edebfa71866b6b2e6fe3ed7c3179fb62
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-01 01:53:22 +0700

    fix mai

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/api/products', [ProductController::class, 'index']);   
+Route::get('/products', [ProductController::class, 'index']);   
