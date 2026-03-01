cb3b02e477999a1c72c635d82a69b85f17489e54
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-01 14:15:01 +0700

    fixxxxx

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,10 +3,8 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/products', [ProductController::class, 'index']);   
-Route::get('/products', function () {
-    return response()->json([
-        'cors_config' => config('cors'),
-        'middleware' => app()->make(\Illuminate\Contracts\Http\Kernel::class),
-    ]);
+Route::get('/products', [ProductController::class, 'index']); 
+// Thêm route test riêng
+Route::get('/cors-test', function () {
+    return response()->json(config('cors'));
 });
