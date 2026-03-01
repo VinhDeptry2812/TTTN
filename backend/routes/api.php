d12c80f9571885e26502191653abc60b5bfc8c22
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-01 14:36:52 +0700

    fix

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,3 +8,7 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/cors-test', function () {
     return response()->json(config('cors'));
 });
+
+Route::get('/version', function () {
+    return response()->json(['version' => '2.0', 'time' => now()]);
+});
