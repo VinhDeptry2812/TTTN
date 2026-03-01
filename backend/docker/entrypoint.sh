2fa09e546975c19bd8060eca457cd8d02d727ff3
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-01 15:49:59 +0700

    fix trắng BE /

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -15,6 +15,7 @@ php artisan cache:clear || true
 php artisan route:clear || true
 php artisan view:clear || true
 php artisan config:cache || true
+php artisan view:cache || true
 
 echo "🚀 Starting Nginx + PHP-FPM..."
 php-fpm -D
