e94b8bc652fda5937efd1f37274a1a22ecac4fcb
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-01 14:01:57 +0700

    da

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,10 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [ProductController::class, 'index']);   
+//Route::get('/products', [ProductController::class, 'index']);   
+Route::get('/products', function () {
+    return response()->json([
+        'cors_config' => config('cors'),
+        'middleware' => app()->make(\Illuminate\Contracts\Http\Kernel::class),
+    ]);
+});
