17da4ae2b7acb10e3fc3fa4d6d09ee361c6e7ec5
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-01 21:55:06 +0700

    fix login/register

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -16,6 +16,7 @@ php artisan route:clear || true
 php artisan view:clear || true
 php artisan config:cache || true
 php artisan view:cache || true
+RUN php artisan l5-swagger:generate || true
 
 echo "🚀 Starting Nginx + PHP-FPM..."
 php-fpm -D
