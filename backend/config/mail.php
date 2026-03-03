a1e2a6758259eba47322b6c4886397fec5315e4e
Vinhdeptry <vinhimpact2812@gmail.com>
2026-03-03 22:28:00 +0700

    reset pass mailtrap

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -65,6 +65,17 @@ return [
         'resend' => [
             'transport' => 'resend',
         ],
+        'mailtrap' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'sandbox.smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+        ],
+
+
 
         'sendmail' => [
             'transport' => 'sendmail',
